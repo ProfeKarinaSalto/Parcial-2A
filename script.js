d2f585589942dfea4cd01ade300251224b09e41f
KSalto <karina.salto@sanjosempd.edu.ar>
2026-07-02 19:25:53 -0300

    Parcial 2A Base

---
+++ script.js
@@ -0,0 +1,113 @@
+/* ===================================================== */
+
+/*              COMPLETAR LAS FUNCIONES                  */
+
+/* ===================================================== */
+
+
+function guardarLibro(){
+
+
+
+}
+
+
+
+function mostrarLibros(){
+
+
+
+}
+
+
+
+
+
+
+/* ===================================================== */
+
+/*             BIBLIOTECA DE COMPONENTES JAVASCRIPT          */
+
+/* Utilizar solamente las instrucciones necesarias      */
+
+/* ===================================================== */
+
+let titulo;
+
+let libros=[];
+
+let contenido="";
+
+document.getElementById()
+
+.value
+
+.innerHTML
+
+"titulo"
+
+"mensaje"
+
+"libros"
+
+=
+
+trim()
+
+""
+
+if(){
+
+}
+
+return false;
+
+"Debe ingresar el título."
+
+"Libro registrado correctamente."
+
+libros.push("📘 El Principito");
+
+libros.push("📗 Don Quijote");
+
+libros.push("📙 Cien años de soledad");
+
+for(
+
+let i=0;
+
+i<libros.length;
+
+i++
+
+){
+
+}
+
+contenido +=
+
+"<div class='card'>";
+
+contenido +=
+
+"<h3>";
+
+contenido +=
+
+libros[i];
+
+contenido +=
+
+"</h3>";
+
+contenido +=
+
+"</div>";
+
+contenido +=
+
+"<p><strong>Cantidad de libros: "
+
++ libros.length +
+
+"</strong></p>";
